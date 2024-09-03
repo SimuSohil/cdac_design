1f8590cdc88bfd9651ab7c414a7260a29402b483
Simu_Sohil_R <151726410+SimuSohil@users.noreply.github.com>
2024-09-03 15:46:10 +0530

    Twilio call record test

---
+++ frontend/build/twilio_voice/generated/source/buildConfig/debug/com/twilio/twilio_voice/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.twilio.twilio_voice;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.twilio.twilio_voice";
+  public static final String BUILD_TYPE = "debug";
+}
